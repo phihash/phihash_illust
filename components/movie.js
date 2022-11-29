5e99ffac0fb7bea89c8fa6d6d36eb6be009a8e3c
phihash <insharp0220@gmail.com>
2022-11-29 19:44:57 +0900

    movie調整

--- components/movie.js
+++ components/movie.js
@@ -5,8 +5,8 @@ export default function Movie(){
   return (
     <div className={`${styles.color} ${styles.padding_6}`}>
       <h2>Movie</h2>
-      <div>
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/DGaRoCg4wjI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div >
+       <iframe className={styles.movie_css} src="https://www.youtube.com/embed/DGaRoCg4wjI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
   )
