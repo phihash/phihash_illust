b0e97f66ac084b1c59dde9c4d04409d0b1d9dede
phihash <insharp0220@gmail.com>
2022-12-01 00:46:42 +0900

    OGP設定

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,9 @@ class MyDocument extends Document {
       <Html>
         <Head>
         <title>phihash-illust</title>
+        <meta property="og:description" content="phihashのイラストサイト" />
+        <meta property="og:site_name" content="phihash-illust" />
+        <meta property="og:image" content="hero.PNG" />
         <link rel="apple-touch-icon" href="favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Noto+Sans+JP:wght@700&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&display=swap" rel="stylesheet" />
