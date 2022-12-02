e1206e306698c198d640cff10d043ee58c006a95
phihash <insharp0220@gmail.com>
2022-12-02 18:00:27 +0900

    apple-touch-icon変更

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
         <title>phihash-illust</title>
+
         <meta property="og:description" content="phihashのイラストサイト" />
         <meta property="og:site_name" content="phihash-illust" />
         <meta property="og:image" content="hero.PNG" />
-        <link rel="apple-touch-icon" href="favicon.ico" />
+        <link rel="apple-touch-icon" href="apple.PNG" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Noto+Sans+JP:wght@700&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&display=swap" rel="stylesheet" />
         </Head>
