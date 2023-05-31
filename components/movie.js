97b61bd8edefed22d414bf793ae92e3343c10898
phihash <insharp0220@gmail.com>
2023-05-31 17:20:55 +0900

    lazy loading

--- components/movie.js
+++ components/movie.js
@@ -6,10 +6,8 @@ export default function Movie() {
     <div class="text-center lg:w-2/3 w-full">
         <h1 class="title-font sm:text-4xl text-4xl mb-12 font-medium text-gray-900">Movie</h1>
       </div>
-    <iframe class="lg:w-4/6 md:w-5/6  w-5/6 object-cover object-center rounded" height="240" src="https://www.youtube.com/embed/DGaRoCg4wjI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-{/* lg:w-4/6 md:w-5/6 w-5/6 */}
-      {/* <div class="container px-5 py-24 mx-auto">
-      </div> */}
+    <iframe class="lg:w-4/6 md:w-5/6  w-5/6 object-cover object-center rounded" height="240" src="https://www.youtube.com/embed/DGaRoCg4wjI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" loading="lazy" allowfullscreen></iframe>
+
     </div>
   </section>
   );
